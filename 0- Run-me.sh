c02f9556e1c135c4a2441ba5e5675db17ec0bf62
sumancvb <sumancvb@gmail.com>
2019-01-28 14:08:52 +0530

    Create 0- Run-me.sh

---
+++ 0- Run-me.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+##################################################
+# Title: GNU Radio Env Setup Scripts (set permissions for all other files)
+# Author: Suman Kumar Das
+# Generated: Wed Nov  1 11:52:29 2017
+##################################################
+
+sudo chmod +x 1beforeboot.sh
+sudo chmod +x 2afterboot.sh
+sudo chmod +x 3installgnuradio.sh
+sudo chmod +x 4installdvbs.sh
+sudo chmod +x 5installdvbs2.sh
+sudo chmod +x 6installdvbt2.sh
+sudo chmod +x 7installuhd.sh
+sudo chmod +x 8set_uhd_env.sh
