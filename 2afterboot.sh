4181b66425de60a87e34c38f44210a88bb179fee
sumancvb <sumancvb@gmail.com>
2019-02-12 16:27:01 +0530

    Delete 2afterboot.sh

--- 2afterboot.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-##################################################
-# Title: GNU Radio Env Setup Scripts (After Boot)
-# Author: Suman Kumar Das
-# Generated: Wed Nov  1 11:52:29 2017
-##################################################
-sudo apt-get update
